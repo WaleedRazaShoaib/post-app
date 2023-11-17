33168ccce152a20779319a2f94f75d933cae64f5
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-11-17 07:33:45 -0800

    post app

---
+++ app.js
@@ -0,0 +1,41 @@
+var backgroundImage = "";
+
+function addpost() {
+    var title = document.getElementById("title");
+    var description = document.getElementById("description");
+    // console.log("bgImage", backImage)
+    if (title.value.trim() && description.value.trim()) {
+        var posts = document.getElementById("posts");
+        posts.innerHTML += `<div class="card mb-2">
+<div class="card-header">
+    @POST
+</div>
+<div style = "background-image: url(${backgroundImage})" class="card-body">
+    <blockquote class="blockquote mb-0">
+        <p>${title.value}</p>
+        <footer class="blockquote-footer">${description.value}</cite></footer>
+    </blockquote>
+</div>
+</div>
+`
+        title.value = "";
+        description.value = "";
+
+    }
+    else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'add description and title!',
+        })
+    }
+}
+
+function selectimage(src) {
+    backgroundImage = src ;
+    var bgImage = document.getElementsByClassName("bg-image");
+    for (var i = 0; i < bgImage.length; i++) {
+        bgImage[i].className = "bg-image";
+    }
+    event.target.className += " images-list-selected";
+}
